c5c0952300a2b14c8155c353c888db4f55caf0ec
laurent <kanailaurent@gmail.com>
2022-02-22 18:33:18 +0100

    init

---
+++ src/test/java/com/winamax/golf/GolfTest.java
@@ -0,0 +1,46 @@
+package com.winamax.golf;
+
+import org.junit.jupiter.api.Test;
+
+public class GolfTest {
+
+    @Test
+    public void test1()
+    {
+        //taille: 2 1
+        //ligne: 1H
+        //attendu: >.
+    }
+    @Test
+    public void test2()
+    {
+        //taille: 3 3
+        //ligne: 2.X
+        //ligne: ..H
+        //ligne: .H1
+        //attendu: v..
+    }
+    @Test
+    public void test3()
+    {
+        //taille: 5 5
+        //ligne: 4..XX
+        //ligne: .H.H.
+        //ligne: ...H.
+        //ligne: .2..2
+        //ligne: .....
+        //attendu: v....
+    }
+    @Test
+    public void test4()
+    {
+        //taille: 6 6
+        //ligne: 3..H.2
+        //ligne: .2..H.
+        //ligne: ..H..H
+        //ligne: .X.2.X
+        //ligne: ......
+        //ligne: 3..H..
+        //">>>..v"
+    }
+}
