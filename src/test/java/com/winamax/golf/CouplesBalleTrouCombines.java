d1324b29d03844bbb018836b41ab5f26064e56fb
laurent <kanailaurent@gmail.com>
2022-03-06 12:50:12 +0100

    j'ai crée un objet pour les combinaisons avec typage mais la solution n'est pas là. C'est à l'ordinateur d'essayer toutes les combinaisons. Pas à moi de lui fournir la liste des combinaisons balle-trous

--- src/test/java/com/winamax/golf/CouplesBalleTrouCombines.java
+++ src/test/java/com/winamax/golf/CouplesBalleTrouCombines.java
@@ -41,17 +41,5 @@ public class CouplesBalleTrouCombines {
     }
 
 
-    public class CoupleBalleTrou {
-    public final Trou trou;
-    public final Ball balle;
 
-    public CoupleBalleTrou(Ball ball, Trou trou) {
-        this.balle = ball;
-        this.trou = trou;
-    }
-    @Override
-    public String toString() {
-        return "CoupleBalleTrou{balle=" + balle.x +","+balle.y+",trou=" + trou.x+","+trou.y +'}';
-    }
-}
 }
